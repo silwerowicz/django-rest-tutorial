17f8eac80bc3741f17de44352731cdd2d4f8e838
silwerowicz <pkisztelinski@gmail.com>
2015-01-22 21:31:21 +0100

    create login panel in browser

--- rest_tutorial/urls.py
+++ rest_tutorial/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include
 
 urlpatterns = patterns('',
     url(r'^', include('snippets.urls')),
 
 )
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
